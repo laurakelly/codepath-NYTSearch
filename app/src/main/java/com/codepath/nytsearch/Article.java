669c219838269232b716a6806de766c963b038a1
Laura Kelly <laura.kelly@airbnb.com>
2016-08-08 14:37:20 -0700

    Add basic search app

---
+++ app/src/main/java/com/codepath/nytsearch/Article.java
@@ -0,0 +1,62 @@
+package com.codepath.nytsearch;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by laura_kelly on 8/8/16.
+ */
+public class Article implements Serializable {
+  String webUrl;
+
+  public String getWebUrl() {
+    return webUrl;
+  }
+
+  public String getHeadline() {
+    return headline;
+  }
+
+  public String getThumbnail() {
+    return thumbnail;
+  }
+
+  String headline;
+  String thumbnail;
+
+  public Article(JSONObject jsonObject) {
+    try {
+      this.webUrl = jsonObject.getString("web_url");
+      this.headline = jsonObject.getJSONObject("headline").getString("main");
+
+      JSONArray multimedia = jsonObject.getJSONArray("multimedia");
+
+      if (multimedia.length() > 0) {
+        JSONObject multimediaJson = multimedia.getJSONObject(0);
+        this.thumbnail = "http://www.nytimes.com/" + multimediaJson.getString("url");
+      } else {
+        this.thumbnail = "";
+      }
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static ArrayList<Article> fromJSONArray(JSONArray array) {
+    ArrayList<Article> results = new ArrayList<>();
+
+    for (int x = 0; x < array.length(); x++) {
+      try {
+        results.add(new Article(array.getJSONObject(x)));
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return results;
+  }
+}
